084e5e8ade333f195e47d0e345653b4f5929c0e8
Christian Moronta <cmoronta26@gmail.com>
2018-10-27 15:18:35 -0400

    Add google credentials key

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+
 app.post('/searchForProduct', (req, res) => {
     let image = req.body
     console.log(image);
