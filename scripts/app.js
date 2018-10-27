137ca289a858ae084b01dca7acafac3eca02bb2e
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-10-27 13:10:41 -0400

    built photo taking functionality

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+const constraints = {
+  video: true
+};
+
+const video = document.querySelector("video");
+
+navigator.mediaDevices.getUserMedia(constraints).then(stream => {
+  video.srcObject = stream;
+});
+
+const captureVideoButton = document.querySelector("#screenshot.capture-button");
+const screenshotButton = document.querySelector("#screenshot-button");
+// const img = document.querySelector("#screenshot-target");
+
+const canvas = document.createElement("canvas");
+
+screenshotButton.onclick = video.onclick = function() {
+  // Other browsers will fall back to image/png
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext("2d").drawImage(video, 0, 0);
+
+  //create and customize new img element
+  const newImg = document.createElement("img");
+  newImg.classList.add("main-content", "main-button");
+  const screenshotContainer = document.querySelector("#screenshot-container");
+  screenshotContainer.appendChild(newImg);
+  
+  newImg.src = canvas.toDataURL("image/webp");
+};
+
+function handleSuccess(stream) {
+  screenshotButton.disabled = false;
+  video.srcObject = stream;
+}
